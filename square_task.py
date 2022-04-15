7b7aea5262811e00b8a5f01c55964f2dc6f883c3
Pasha <Pashakhurtenko@gmail.com>
2022-04-15 18:14:45 +0300

    Done task

--- square_task.py
+++ square_task.py
@@ -1,4 +1,7 @@
-def random_square():
-    numbers = [1,2,3,4,5,6,7]
-    squares = [1,2,3,4,5,6,7]
+numbers = [1, 2, 3, 4, 5, 6, 7]
+numbers_sum = sum(numbers)
 
+for num in numbers:
+    square = (numbers_sum - num) % 2
+    if square == 0:
+        print(num)
